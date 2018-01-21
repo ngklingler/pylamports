f7f5b32c607a0fa2e2a30390c5bb55f51383ff51
Nicholas Klingler <ngklingler@gmail.com>
2018-01-20 20:57:42 -0500

    update key.py, create hashEncode helper method and self.publicKey

--- pylamports/key.py
+++ pylamports/key.py
@@ -1,13 +1,23 @@
 import secrets
+import hashlib
+
+
+def hashEncode(number):
+    return hashlib.sha256(str(number).encode()).hexdigest()
 
 
 class key:
 
     def __init__(self):
         self.secretKey = []
+        self.publicKey = []
         for i in range(256):
-            self.secretKey.append((secrets.randbits(256),
-                                   secrets.randbits(256)))
+            self.secretKey.append((hex(secrets.randbits(256))[2:],
+                                   hex(secrets.randbits(256))[2:]))
+            self.publicKey.append((hashEncode(self.secretKey[i][0]),
+                                   hashEncode(self.secretKey[i][1])))
+        self.secretKey = tuple(self.secretKey)
+        self.publicKey = tuple(self.publicKey)
 
     def getSecretHex(self):
         secretKeyHex = []
