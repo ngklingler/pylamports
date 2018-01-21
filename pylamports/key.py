c34366850815dd138b3bd47fcf5a6e8ea8056c74
Nicholas Klingler <ngklingler@gmail.com>
2018-01-20 21:02:41 -0500

    update key.py, create method to sign message hashes

--- pylamports/key.py
+++ pylamports/key.py
@@ -6,6 +6,16 @@ def hashEncode(number):
     return hashlib.sha256(str(number).encode()).hexdigest()
 
 
+def hexToBin(hexString):
+    """takes a hex string, converts it to a binary string, trims the '0b'
+    prefix, and pads it with leading zeroes to ensure a 256 bit length"""
+    binaryString = bin(int(hexString, 16))[2:]
+    if len(binaryString) < 256:
+        prefix = "0" * (256 - len(binaryString))
+        binaryString = prefix + binaryString
+    return binaryString
+
+
 class key:
 
     def __init__(self):
@@ -18,3 +28,11 @@ class key:
                                    hashEncode(self.secretKey[i][1])))
         self.secretKey = tuple(self.secretKey)
         self.publicKey = tuple(self.publicKey)
+
+    def signMessageHash(self, messageHash):
+        binString = hexToBin(messageHash)
+        signatureArray = []
+        for i in range(len(binString)):
+            bit = int(binString[i])
+            signatureArray.append(self.secretKey[i][bit])
+        return tuple(signatureArray)
